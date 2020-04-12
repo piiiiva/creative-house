93f28c581a5130f925b8045df7638f35c8006977
PivaDesk <piiiiva@gmail.com>
2020-04-11 23:44:57 -0300

    Create Modal in ideas page reusing html and css codes; CSS Refactoring
    Add/remove scrollbars off idea page when open modal using Javascrip; Add scrollbars inside modal using Javascript; Refactoring CSS of buttons to use just one .class

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function onOff () {
     document.querySelector('#modal').classList.toggle("hide")
+    document.querySelector('body').classList.toggle("hide-scroll")
+    document.querySelector('#modal').classList.toggle("add-scroll")
 }
