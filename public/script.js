c647728a0434de34270bdb72479bc422fa54de3e
PivaDesk <piiiiva@gmail.com>
2020-04-13 23:35:56 -0300

    Using db.js inside methods get in server.js; Creating a method POST to take data of new form and passing all data inside DataBase with req.body avaible in POST Methods; Frontend Validation of form fields with Javascript

--- public/script.js
+++ public/script.js
@@ -3,3 +3,28 @@ function onOff () {
     document.querySelector('body').classList.toggle("hide-scroll")
     document.querySelector('#modal').classList.toggle("add-scroll")
 }
+
+function checkFields(event) {
+    const valuesToCheck = [
+        "title",
+        "category",
+        "image",
+        "description",
+        "link",       
+    ]
+
+
+    const isEmpty = valuesToCheck.find(function(value){
+        const checkIfIsString = typeof event.target[value].value === "string"
+        const checkIfIsEmpty = !event.target[value].value.trim()
+        if (checkIfIsString && checkIfIsEmpty) {
+            return true
+        }
+    })
+
+    if(isEmpty) {
+        event.preventDefault()
+        alert("Por favor preencha todos os campos")
+    }
+
+}
--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const nunjucks = require('nunjucks')
 
 const server = express()
 
+const db =require('./db')
+
 server.use(express.static('public'))
 server.set("view engine", "njk")
+
+// Habilitar uso do req.body
+server.use(express.urlencoded({ extended: true }))
 
 nunjucks.configure('views', {
     express: server,
@@ -12,25 +17,68 @@ nunjucks.configure('views', {
     autoescape: false,
 })
 
-const ideas = require('./dataIdeas')
+// const ideas = require('./dataIdeas')
 
 server.get('/', function(req, res){
 
-    const reversedIdeas = [...ideas].reverse() // [...ideas] é uma cópia da const ideas
-    let lastIdeas = []
-    for (let idea of reversedIdeas) {
-        if (lastIdeas.length < 2) {
-            lastIdeas.push(idea)
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if (err) {
+            console.log(err)
+            return res.send('Erro no banco de dados!')
         }
-    }
 
-    return res.render("index.njk", { ideas: lastIdeas })
+        const reversedIdeas = [...rows].reverse() // [...ideas] é uma cópia da const ideas
+        
+        let lastIdeas = []
+        for (let idea of reversedIdeas) {
+            if (lastIdeas.length < 2) {
+                lastIdeas.push(idea)
+            }
+        }
+
+        return res.render("index.njk", { ideas: lastIdeas })
+        
+    })   
 })
 
 server.get('/ideas', function(req, res){
-    const reversedIdeas = [...ideas].reverse()
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if (err) {
+            console.log(err)
+            return res.send('Erro no banco de dados!')
+        }
 
-    return res.render("ideas.njk", { ideas: reversedIdeas })
+        const reversedIdeas = [...rows].reverse()
+    
+        return res.render("ideas.njk", { ideas: reversedIdeas })
+    })
+})
+
+server.post('/', function(req, res){
+    const query = `INSERT INTO ideas(
+        image,
+        title,
+        category,
+        description,
+        link
+        ) VALUES (?, ?, ?, ?, ?);`
+    
+    const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link
+    ]
+            
+    db.run(query, values, function(err) {
+        if (err) {
+            console.log(err)
+            return res.send('Erro no banco de dados!')
+        }
+
+        return res.redirect('/ideas')
+    })
 })
 
 server.listen(5000, () => {
